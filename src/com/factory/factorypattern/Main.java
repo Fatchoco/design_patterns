dbfb107786342c572271e525076aa90a25acb57a
Erico <erico.i.am@gmail.com>
2020-09-20 09:07:21 +0800

    Factory: Add new lines

--- src/com/factory/factorypattern/Main.java
+++ src/com/factory/factorypattern/Main.java
@@ -6,13 +6,18 @@ public class Main {
 
         Pizza pizza1 = ps.orderPizza("cheese");
         System.out.println(pizza1.getPizzaName());
+        System.out.println(" ");
         Pizza pizza2 = ps.orderPizza("veggie");
         System.out.println(pizza2.getPizzaName());
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
 
         ps = new ChicagoStylePizzaStore();
 
         pizza1 = ps.orderPizza("cheese");
         System.out.println(pizza1.getPizzaName());
+        System.out.println(" ");
         pizza2 = ps.orderPizza("veggie");
         System.out.println(pizza2.getPizzaName());
     }
