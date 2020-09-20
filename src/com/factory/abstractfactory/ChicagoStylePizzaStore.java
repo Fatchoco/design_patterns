ff8f1d3e9473480cc531c736c483af5278a91b75
Erico <erico.i.am@gmail.com>
2020-09-20 20:00:53 +0800

    Factory: AbstractFactory fix ChicagoPizzaStore

--- src/com/factory/abstractfactory/ChicagoStylePizzaStore.java
+++ src/com/factory/abstractfactory/ChicagoStylePizzaStore.java
@@ -3,12 +3,17 @@ package com.factory.abstractfactory;
 public class ChicagoStylePizzaStore extends PizzaStore {
     @Override
     public Pizza createPizza(String type) {
+        PizzaIngredientFactory ingredientFactory = new ChicagoPizzaIngredientFactory();
+
         if (type.equalsIgnoreCase("cheese")) {
-            pizza = new ChicagoStyleCheesePizza();
+            pizza = new CheesePizza(ingredientFactory);
+            pizza.setPizzaName("Chicago Style Cheese Pizza");
         } else if (type.equalsIgnoreCase("clam")) {
-            pizza = new ChicagoStyleClamPizza();
+            pizza = new ClamPizza(ingredientFactory);
+            pizza.setPizzaName("Chicago Style Clam Pizza");
         } else if (type.equalsIgnoreCase("veggie")) {
-            pizza = new ChicagoStyleVeggiePizza();
+            pizza = new VeggiePizza(ingredientFactory);
+            pizza.setPizzaName("Chicago Style Veggie Pizza");
         }
 
         return pizza;
